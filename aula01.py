fafb73fc5027a696829b1717b24791d3d9338992
presotto777 <presotto.joao@escola.pr.gov.br>
2025-04-02 19:14:03 +0000

    concluir aula02 pandas

--- aula01.py
+++ aula01.py
@@ -4,17 +4,17 @@ import matplotlib.pyplot as plt
 
 def ExibirGrafico():
   #definição dos grupos e valores 
-   grupos= ['A', 'B', 'C']
-   valor = [23, 38, 12]
+  grupos= ['A', 'B', 'C']
+  valores = [23, 38, 12]
 
-# configura um grafico de barras, onde recebe os grupos, valores
-# E opcionalmente as cores 
-plt.bar(grupos, valores, color=['red', 'blue', 'grey'])
+  # configura um grafico de barras, onde recebe os grupos, valores
+  # E opcionalmente as cores 
+  plt.bar(grupos, valores, color=['red', 'blue', 'grey'])
 
-# define o titulo gráfico
-plt.title('gráfico de barras simples')
+  # define o titulo gráfico
+  plt.title('gráfico de barras simples')
 
-# define o titulo do eixo x
-plt.xlabel('grupos')
+  # define o titulo do eixo x
+  plt.xlabel('grupos')
 
 
--- aula02.py
+++ aula02.py
@@ -14,7 +14,7 @@ def exibirgrafico():
     plt.figure(figsize=[10.0, 5.0])
 
     #criação do grafico
-    plt.plot(df['meses'], df ['temperatura'],color="green")
+    plt.plot(df['meses'], df['temperaturas'],color="green")
 
     #definição de titulos
     plt.title("temperatura média ao longo do tempo")
@@ -26,4 +26,4 @@ def exibirgrafico():
     plt.savefig('chart2.png')
 
     # exibe no console dados estatisticos
-    print(f'Temperaturas: \n{df['Temperaturas'].describe()}')
+    print(f'Temperaturas: \n{df['temperaturas'].describe()}')
